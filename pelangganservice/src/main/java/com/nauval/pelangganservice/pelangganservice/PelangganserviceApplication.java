cb23914bec4389bcb32731d2825a18eeb80acc03
nauvalalpen <nauvalalpenperdana@gmail.com>
2025-08-30 17:48:26 +0700

    first commit

---
+++ pelangganservice/src/main/java/com/nauval/pelangganservice/pelangganservice/PelangganserviceApplication.java
@@ -0,0 +1,13 @@
+package com.nauval.pelangganservice.pelangganservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PelangganserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PelangganserviceApplication.class, args);
+	}
+
+}
---
+++ pelangganservice/src/main/java/com/nauval/pelangganservice/pelangganservice/repository/PelangganRepository.java
@@ -0,0 +1,12 @@
+package com.nauval.pelangganservice.pelangganservice.repository;
+
+import com.nauval.pelangganservice.pelangganservice.model.Pelanggan;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PelangganRepository extends JpaRepository<Pelanggan, Long> {
+    // JpaRepository sudah menyediakan method seperti findAll(), findById(), save(),
+    // deleteById()
+    // Anda bisa menambahkan query kustom di sini jika diperlukan
+}
