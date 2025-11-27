7ded46c50d5645bde5564ee384cecb2d6bdca7c3
nauvalalpen <nauvalalpenperdana@gmail.com>
2025-11-28 00:26:08 +0700

    feat: Add logging functionality to AnggotaController for better request tracking

--- PerpustakaanService/anggotaservice/src/main/java/com/nauval/anggotaservice/anggotaservice/controller/AnggotaController.java
+++ PerpustakaanService/anggotaservice/src/main/java/com/nauval/anggotaservice/anggotaservice/controller/AnggotaController.java
@@ -3,28 +3,41 @@ package com.nauval.anggotaservice.anggotaservice.controller;
 import com.nauval.anggotaservice.anggotaservice.model.Anggota;
 import com.nauval.anggotaservice.anggotaservice.service.AnggotaService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger; // Import Logger
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api/anggota")
 public class AnggotaController {
+    private static final Logger log = LoggerFactory.getLogger(AnggotaController.class);
     @Autowired
     private AnggotaService anggotaService;
 
     @PostMapping
     @ResponseBody
     public Anggota save(@RequestBody Anggota anggota) {
-        return anggotaService.save(anggota);
+        // 2. Tulis Log saat ada POST request
+        log.info("Menerima request POST anggota baru. Nama: {}, Email: {}", anggota.getNama(), anggota.getEmail());
+
+        Anggota savedAnggota = anggotaService.save(anggota);
+
+        // 3. (Opsional) Tulis Log saat sukses
+        log.info("Sukses menyimpan anggota dengan ID: {}", savedAnggota.getId());
+
+        return savedAnggota;
     }
 
     @GetMapping("/{id}")
     public Anggota findById(@PathVariable Long id) {
+        log.info("Mencari anggota dengan ID: {}", id); // Log GET by ID
         return anggotaService.findById(id);
     }
 
     @GetMapping("/")
     public List<Anggota> findAll() {
+        log.info("Mengambil semua data anggota"); // Log GET All
         return anggotaService.findAll();
     }
 
