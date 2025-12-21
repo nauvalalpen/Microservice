c089e7cf5fa65aced08e6f30b17b79c591b58094
nauvalalpen <nauvalalpenperdana@gmail.com>
2025-12-21 19:02:40 +0700

    fix: Enhance logging message in save method of BukuController for clarity

--- PerpustakaanService/bukuservice/src/main/java/com/nauval/bukuservice/bukuservice/controller/BukuController.java
+++ PerpustakaanService/bukuservice/src/main/java/com/nauval/bukuservice/bukuservice/controller/BukuController.java
@@ -22,7 +22,8 @@ public class BukuController {
     @PostMapping
     public Buku save(@RequestBody Buku buku) {
         // 4. Log saat menerima request POST
-        log.info("Menerima request POST buku baru. Judul: {}, Pengarang: {}", buku.getJudul(), buku.getPengarang());
+        log.info("Menerima request POST buku baru. Judul Buku: {}, Pengarang: {}", buku.getJudul(),
+                buku.getPengarang());
 
         Buku savedBuku = bukuService.save(buku);
 
